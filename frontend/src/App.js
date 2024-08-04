95222a77ef8ffa9fc87ebd7eee590516c20b15a4
Ahmad Jan <ahmadjan200316@gmail.com>
2024-08-05 01:18:50 +0500

    before redirecting to dashboard logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,10 @@
 
 import './App.css';
-import DrawableMap from './components/drawableMap';
-import MyComponent from './components/googlemap';
-import Items from './Items';
+import LandingPage from './components/LandingPage';
 function App() {
   return (
     <div className="App">
-      {/* <MyComponent></MyComponent> */}
-      <DrawableMap></DrawableMap>
+      <LandingPage></LandingPage>
     </div>
   );
 }
---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import DrawableMap from './drawableMap'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <DrawableMap></DrawableMap>
+    </div>
+  )
+}
+
+export default Dashboard
