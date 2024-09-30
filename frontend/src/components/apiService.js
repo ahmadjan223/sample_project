d3402b7f025ecbc7197200da3df4e0702be2e330
Shaheer Kashif <shaheerkashif02@gmail.com>
2024-09-30 17:44:58 +0500

    asdfghjk

--- frontend/src/components/apiService.js
+++ frontend/src/components/apiService.js
@@ -1,7 +1,7 @@
 export const savePolygon = async (coordinates, name, userId) => {
     try {
       const response = await fetch(
-        "https://densefusion-3n1o.vercel.app/api/save-single-polygon",
+        "https://densefusion-3n10.vercel.app/api/save-single-polygon",
         {
           method: "POST",
           headers: {
--- frontend/src/components/sidenav.js
+++ frontend/src/components/sidenav.js
@@ -32,7 +32,7 @@ const SideNav = ({
 
   // Handle logout
   const handleLogout = () => {
-    window.location.href = "https://densefusion-3n1o.vercel.app/api/logout";
+    window.location.href = "https://densefusion-3n10.vercel.app/api/logout";
   };
 
   // Function to open the FieldDetails page
