07bd126a0535417790d28e19b9393d3244a719f2
Ahmad Jan <ahmadjan200316@gmail.com>
2024-10-01 19:27:04 +0500

    dummy user is added for testing vercel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,27 @@ import React, { useEffect, useState } from 'react';
 import Dashboard from './components/Dashboard';
 import LandingPage from './components/LandingPage';
 
+
 const App = () => {
-    const [user, setUser] = useState(null);
-    useEffect(() => {
-        const query = new URLSearchParams(window.location.search);
-        const name = query.get('name');
-        const id = query.get('id');
-        const image = query.get('image');
-
-        if (name && id && image) {
-            setUser({ displayName: name, id, image });
-        }
-    }, []);
-
+    const [user, setUser] = useState({
+        displayName: "Ahmad Jan",
+        id: "66afae5f5f3f07e8eb7826f6",
+        image: "https://lh3.googleusercontent.com/a/ACg8ocIKoE8kx__kPERJcJ14hL3QhCMe2Vy1v0OKyortYvVwzJFe0eiN=s96-c",
+      });
+    // useEffect(() => {
+    //     const query = new URLSearchParams(window.location.search);
+    //     const name = query.get('name');
+    //     const id = query.get('id');
+    //     const image = query.get('image');
+    //     if (name && id && image) {
+    //         setUser({ displayName: name, id, image });
+    //     }
+    // }, []);
+    // useEffect(() => {
+    //     if(user){
+    //         console.log("dummy data",user)
+    //     }
+    // },[user])
     if (user) {
         return <Dashboard user={user} />;
     }
