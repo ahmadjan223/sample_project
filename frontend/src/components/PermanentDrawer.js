a4d9dcd2ff80b13c5fe0c7bb652d71dc39d42c5e
Ahmad Jan <ahmadjan200316@gmail.com>
2024-10-17 00:26:18 +0500

    add field and cancel button added

--- frontend/src/components/PermanentDrawer.js
+++ frontend/src/components/PermanentDrawer.js
@@ -18,7 +18,6 @@ import { createTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
-
 const drawerWidth = 340;
 const topBarHeight = 64;
 
@@ -34,6 +33,7 @@ const PermanentDrawer = ({
   const [polygonInfo, setPolygonInfo] = useState([]);
   const [open, setOpen] = useState(false);
   const [selectedFieldCoords, setSelectedFieldCoords] = useState();
+  const [addField, setAddField] = useState(false);
 
   const handlePolygons = () => {
     const formattedPolygons = polygons.map((polygon, index) => ({
@@ -110,62 +110,66 @@ const PermanentDrawer = ({
 
     typography: {
       allVariants: {
-        fontFamily: 'serif',
+        fontFamily: "serif",
       },
     },
-
   });
 
   const handleAddField = () => {
+    setAddField(!addField);
     setSnackBarOpen(true);
     setIsDrawing(true);
+  };
+  const handleCancelField = () => {
+    setAddField(!addField);
+    setSnackBarOpen(false);
+    setIsDrawing(false);
   };
 
   return (
     <>
       <ThemeProvider theme={darkTheme}>
-
-          <DetailsDrawer
-            open={open}
-            setOpen={setOpen}
-            drawerWidth={drawerWidth}
-            topBarHeight={topBarHeight}
-            goBackToSidebar={goBackToSidebar}
-            selectedFieldName={selectedFieldName}
-            setSelectedFieldName={setSelectedFieldName}
-            selectedFieldCoords={selectedFieldCoords}
-            handleEditField={handleEditField}
-            DataFetch={DataFetch}
-            polygons={polygons}
-            polygonInfo={polygonInfo}
-          />
-          <Drawer
-            sx={{
+        <DetailsDrawer
+          open={open}
+          setOpen={setOpen}
+          drawerWidth={drawerWidth}
+          topBarHeight={topBarHeight}
+          goBackToSidebar={goBackToSidebar}
+          selectedFieldName={selectedFieldName}
+          setSelectedFieldName={setSelectedFieldName}
+          selectedFieldCoords={selectedFieldCoords}
+          handleEditField={handleEditField}
+          DataFetch={DataFetch}
+          polygons={polygons}
+          polygonInfo={polygonInfo}
+        />
+        <Drawer
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            marginTop: `${topBarHeight}px`, // Push drawer below TopBar
+            "& .MuiDrawer-paper": {
               width: drawerWidth,
-              flexShrink: 0,
-              marginTop: `${topBarHeight}px`, // Push drawer below TopBar
-              "& .MuiDrawer-paper": {
-                width: drawerWidth,
-                boxSizing: "border-box",
-                marginTop: `${topBarHeight}px`, // Apply margin to drawer paper
-              },
+              boxSizing: "border-box",
+              marginTop: `${topBarHeight}px`, // Apply margin to drawer paper
+            },
+          }}
+          variant="permanent"
+          anchor="left"
+        >
+          {/* top div */}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              height: "90%", // Ensures the container takes the full height of the sidebar
             }}
-            variant="permanent"
-            anchor="left"
           >
-            {/* top div */}
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "space-between",
-                height: "90%", // Ensures the container takes the full height of the sidebar
-              }}
-            >
-              {/* div 1 for fields info */}
-              <div>
-                {/* this div is for add button */}
-                <div
+            {/* div 1 for fields info */}
+            <div>
+              {/* this div is for add button */}
+              {/* <div
                   style={{
                     border: "10px solid transparent",
                     display: "flex",
@@ -197,58 +201,91 @@ const PermanentDrawer = ({
                       </Fab>
                     </Box>
                   </div>
-                </div>
+                </div> */}
+              {addField ? (
+                <Button
+                  sx={{
+                    color: "white",
+                    backgroundColor: "green",
+                    width: "calc(100% - 32px)", // Adjust width to account for right margin
+                    height: "50px",
+                    fontSize: "18px",
+                    fontWeight: "bold",
+                    margin: "16px",
+                  }}
+                  onClick={handleAddField}
+                  variant="contained"
+                >
+                  Add new Field
+                </Button>
+              ) : (
+                <Button
+                  sx={{
+                    color: "white",
+                    backgroundColor: "red",
+                    width: "calc(100% - 32px)", // Adjust width to account for right margin
+                    height: "50px",
+                    fontSize: "18px",
+                    fontWeight: "bold",
+                    margin: "16px",
+                  }}
+                  onClick={handleCancelField}
+                  variant="contained"
+                >
+                  Cancel
+                </Button>
+              )}
 
-                <Divider />
+              <Divider />
 
-                <List>
-                  {polygonInfo.map((field) => (
-                    <ListItem key={field.name} disablePadding>
-                      <ListItemButton
-                        onClick={() => {
-                          // setSelectedFieldName(field.name);
-                          openDetailsPage(field.name);
-                        }}
-                      >
-                        <ListItemIcon>
-                          <LayersIcon />
-                        </ListItemIcon>
+              <List>
+                {polygonInfo.map((field) => (
+                  <ListItem key={field.name} disablePadding>
+                    <ListItemButton
+                      onClick={() => {
+                        // setSelectedFieldName(field.name);
+                        openDetailsPage(field.name);
+                      }}
+                    >
+                      <ListItemIcon>
+                        <LayersIcon />
+                      </ListItemIcon>
 
-                        <ListItemText primary={field.name} />
-                      </ListItemButton>
-                    </ListItem>
-                  ))}
-                </List>
-              </div>
+                      <ListItemText primary={field.name} />
+                    </ListItemButton>
+                  </ListItem>
+                ))}
+              </List>
+            </div>
 
-              {/* div 2 for user info */}
-              <div>
-                {/* This div is for user info */}
-                <div style={{ padding: "16px" }}>
-                  <div
-                    style={{
-                      display: "flex",
-                      alignItems: "center",
-                      gap: "8px",
-                    }}
-                  >
-                    <Avatar src="avatar.png" />
-                    <div>
-                      <Typography variant="h6" color="white">
-                        {user.displayName}
-                      </Typography>
-                      <Typography variant="body2" color="gray">
-                        {user.id}
-                      </Typography>
-                    </div>
-                    <Button onClick={handleLogout} color="inherit">
-                      Logout
-                    </Button>
+            {/* div 2 for user info */}
+            <div>
+              {/* This div is for user info */}
+              <div style={{ padding: "16px" }}>
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "8px",
+                  }}
+                >
+                  <Avatar src="avatar.png" />
+                  <div>
+                    <Typography variant="h6" color="white">
+                      {user.displayName}
+                    </Typography>
+                    <Typography variant="body2" color="gray">
+                      {user.id}
+                    </Typography>
                   </div>
+                  <Button onClick={handleLogout} color="inherit">
+                    Logout
+                  </Button>
                 </div>
               </div>
             </div>
-          </Drawer>
+          </div>
+        </Drawer>
       </ThemeProvider>
     </>
   );
